86b56efedbf8c69e2b8408835ee13a0965e4bebd
Didafe <dida_fernando@hotmail.com>
2019-12-23 04:01:14 -0300

    Update index.html/calcController.js

--- scripts/controller/calcController.js
+++ scripts/controller/calcController.js
@@ -3,9 +3,24 @@ class CalcController{
     constructor(){
 
         this._displayCalc = "0";
-        this_currentDate;
+        this._currentDate;
+        this.initialize();
 
     }
+
+    initialize(){
+
+        let displayCalcEl = document.querySelector("#display");
+        let dateEl = document.querySelector("#data");
+        let timeEl = document.querySelector("#hora");
+
+        displayCalcEl.innerHTML = "4567";
+        dateEl.innerHTML = "01/05/2020";
+        timeEl.innerHTML = "00:00";
+
+    }
+
+
 
     get displayCalc(){
         return this._displayCalc;
